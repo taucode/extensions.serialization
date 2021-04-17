f33db5ec439b03b5a18c5919ebf805ab541703d9
Andrey Kovalenko <ak@taucode.com>
2021-04-17 17:42:57 +0300

    Added solution files.

---
+++ src/TauCode.Extensions.Serialization/SerializationExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Reflection;
+
+namespace TauCode.Extensions.Serialization
+{
+    public static class SerializationExtensions
+    {
+        public static T GetDeserializedJson<T>(
+            this Assembly assembly,
+            string resourceName,
+            bool findFullName = false)
+        {
+            if (findFullName)
+            {
+                resourceName = assembly.FindFullResourceName(resourceName);
+            }
+
+            var json = assembly.GetResourceText(resourceName, findFullName);
+            var result = JsonConvert.DeserializeObject<T>(json);
+            return result;
+        }
+
+        public static string NormalizeJson(string json)
+        {
+            var obj = JsonConvert.DeserializeObject(json);
+            var normalizedJson = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            return normalizedJson;
+        }
+    }
+}
