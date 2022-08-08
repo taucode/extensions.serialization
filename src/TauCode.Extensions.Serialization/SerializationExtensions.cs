4afeb05b7be62450538309501f8d5fcb8eac7ac5
Andrey Kovalenko <ak@taucode.com>
2022-08-08 20:14:43 +0300

    Updated content for 1.5.0.

--- src/TauCode.Extensions.Serialization/SerializationExtensions.cs
+++ src/TauCode.Extensions.Serialization/SerializationExtensions.cs
@@ -1,30 +1,29 @@
 ﻿using Newtonsoft.Json;
 using System.Reflection;
 
-namespace TauCode.Extensions.Serialization
+namespace TauCode.Extensions.Serialization;
+
+public static class SerializationExtensions
 {
-    public static class SerializationExtensions
+    public static T GetDeserializedJson<T>(
+        this Assembly assembly,
+        string resourceName,
+        bool findFullName = false)
     {
-        public static T GetDeserializedJson<T>(
-            this Assembly assembly,
-            string resourceName,
-            bool findFullName = false)
+        if (findFullName)
         {
-            if (findFullName)
-            {
-                resourceName = assembly.FindFullResourceName(resourceName);
-            }
-
-            var json = assembly.GetResourceText(resourceName, findFullName);
-            var result = JsonConvert.DeserializeObject<T>(json);
-            return result;
+            resourceName = assembly.FindFullResourceName(resourceName);
         }
 
-        public static string NormalizeJson(string json)
-        {
-            var obj = JsonConvert.DeserializeObject(json);
-            var normalizedJson = JsonConvert.SerializeObject(obj, Formatting.Indented);
-            return normalizedJson;
-        }
+        var json = assembly.GetResourceText(resourceName, findFullName);
+        var result = JsonConvert.DeserializeObject<T>(json);
+        return result;
+    }
+
+    public static string NormalizeJson(string json)
+    {
+        var obj = JsonConvert.DeserializeObject(json);
+        var normalizedJson = JsonConvert.SerializeObject(obj, Formatting.Indented);
+        return normalizedJson;
     }
 }
